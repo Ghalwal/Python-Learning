e5cbe04225f207e20f8149d3379e9a97a924108f
Ghalwal <136774468+Ghalwal@users.noreply.github.com>
2023-06-16 05:58:32 +0300

    Add files via upload

---
+++ pythgame.py
@@ -0,0 +1,20 @@
+print("Welcome to my python game!")
+game = input("Do u want to play?:")
+
+if game != "yes":
+    quit()
+print("Okay! lets game :)")
+
+answer = input("Whats your name?")
+if answer == "ghalia":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+playing = input("How old are you?:")
+if playing == "twenty three":
+    print("Correct!")
+else:
+    print("incorrect!")
+
+print("congrats u won the game")
